4694622e1aa0919f649299f4a8c9a2044ccdf045
Vladimir Litvinchik <litvinchik.vladimir@gmail.com>
2016-10-04 20:16:31 +0300

    Added missing files.

---
+++ test/OutputSaveStreamTests.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace GroupDocs.Viewer.AWS.S3.Tests
+{
+    [TestFixture]
+    public class OutputSaveStreamTests
+    {
+        [Test]
+        public void ShouldCallDelegateAfterBeforeDisposed()
+        {
+            bool wasCalled = false;
+
+            OutputSaveStream outputSaveStream = new OutputSaveStream(delegate(Stream stream)
+            {
+                wasCalled = true;
+                return true;
+            });
+
+            outputSaveStream.Dispose();
+
+            Assert.IsTrue(wasCalled);
+        }
+
+        [Test]
+        public void ShouldCallDelegateAfterBeforeClosed()
+        {
+            bool wasCalled = false;
+
+            OutputSaveStream outputSaveStream = new OutputSaveStream(delegate (Stream stream)
+            {
+                wasCalled = true;
+                return true;
+            });
+
+            outputSaveStream.Close();
+
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
